2877f5fbba56bee67b584c1ee821c308548d121c
brighterfrog <brighterfrog@gmail.com>
2021-11-06 19:01:20 -0500

    terraform add elastic env

--- application/tf/main.tf
+++ application/tf/main.tf
@@ -1,4 +1,4 @@
-terraform {    
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,7 +7,7 @@ terraform {
   }
 
   required_version = "1.0.10"
-  backend "s3" {}   #comment out to initialize  
+  backend "s3" {} #comment out to initialize  
 }
 
 provider "aws" {
@@ -15,19 +15,45 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_dynamodb_table" "dynamodb-terraform-lock" {
-   name = "tf-adexr-states-locks"
-   hash_key = "LockID"
-   read_capacity = 20
-   write_capacity = 20
+# resource "aws_dynamodb_table" "dynamodb-terraform-lock" {
+#   name           = "tf-adexr-states-locks"
+#   hash_key       = "LockID"
+#   read_capacity  = 20
+#   write_capacity = 20
 
-   attribute {
-      name = "LockID"
-      type = "S"
-   }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
 
-   tags = {
-     Name = "Terraform Lock Table"
-   }
+#   tags = {
+#     Name = "Terraform Lock Table"
+#   }
+# }
+
+
+module "elastic_beanstalk" {
+  source  = "./modules/elastic-beanstalk"
+  globals = var.globals
 }
 
+
+# resource "aws_elastic_beanstalk_application" "backend_ticker_subscriber" {
+#   name        = "backend-eventlistener-adexrio"
+#   description = "backend-eventlistener-adexrio"
+
+#   appversion_lifecycle {
+#            delete_source_from_s3 = false
+#            max_age_in_days       = 0 
+#            max_count             = 10
+#            service_role          = "arn:aws:iam::891289117461:role/aws-elasticbeanstalk-service-role"
+#         }
+# }
+
+# resource "aws_elastic_beanstalk_environment" "beanstalk_env" {
+#   name                = var.globals[terraform.workspace].aws_elastic_beanstalk_environment_name
+#   description        = "adexr.io backend blockchain ticker"
+#   application         = aws_elastic_beanstalk_application.backend_ticker_subscriber.name
+#   solution_stack_name = "64bit Amazon Linux 2 v5.4.7 running Node.js 14"
+#   tier                = "WebServer" 
+# }
--- application/tf/variables.tf
+++ application/tf/variables.tf
@@ -1,3 +1,18 @@
 variable "aws_region" {
-    default = "us-east-1"
+  default = "us-east-1"
+}
+
+variable "globals" {
+  type = map(any)
+  default = {
+    "dev" = {
+      "aws_elastic_beanstalk_environment_name" = "Backendeventlisteneradexrio-DEV"
+    }
+    "test" = {
+      "aws_elastic_beanstalk_environment_name" = "Backendeventlisteneradexrio-TEST"
+    }
+    "prod" = {
+      "aws_elastic_beanstalk_environment_name" = "Backendeventlisteneradexrio-PROD"
+    }
+  }
 }
