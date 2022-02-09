880d4c06f4c891a2813cb3a2796fc25050909802
brighterfrog <brighterfrog@gmail.com>
2022-02-08 21:47:21 -0600

    copy historical to block event processor

--- application/typescript/lambda_block_event_processor/src/index.ts
+++ application/typescript/lambda_block_event_processor/src/index.ts
@@ -1,28 +1,10 @@
-"use strict";
-//const stepFnService = require('./services/step-function-service');
+'use strict';
+import { Orchestrator } from "./services/orchestrator";
 
-export const handler = async (event: any, context: any): Promise<any> => {      
-  console.log('lambda block event processor started with event ', event);  
-  
-  // const response = await stepFnService.startStepFn(event, context);  
+ export const handler = async (event: any, context: any): Promise<any> => {
+    
+    console.log('lambda block event processor ', event);
 
-  console.log('DONE');
+    await (new Orchestrator()).process(event);           
 
-  //FOR BLOCK
-  //GET ALL CREATE EVENTS
-    //check if record already exists, if does, skip, else, add
-    //WRITE TO TABLE
-  //GET PLAYER JOINED EVENTS
-    //check if record already exists, if does, skip, else, add
-    //WRITE TO TABLE
-  //GET PLAYER LEFT EVENTS
-    //check if record already exists, if does, skip, else, add
-    //WRITE TO TABLE
-  //GET COMPLETED POOL EVENTS
-    //check if record already exists, if does, skip, else, add  
-    //WRITE TO TABLE
-  //GET AWAIT EXECUTION EVENTS
-    //check if record already exists, if does, skip, else, add
-    //WRITE TO TABLE
-  
 };
